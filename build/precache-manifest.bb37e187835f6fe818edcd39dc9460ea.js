5086467e4c2ab0d608076793c130669049414c1b
yamin zann <68142796+yaminzan9@users.noreply.github.com>
2021-01-24 21:21:44 +0800

    edting

---
+++ build/precache-manifest.bb37e187835f6fe818edcd39dc9460ea.js
@@ -0,0 +1,158 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6fd302c7ed84c71a9a4fb47fcf5d3ef",
+    "url": "/Get-Movies-Info/index.html"
+  },
+  {
+    "revision": "6736827938ed82624360",
+    "url": "/Get-Movies-Info/static/css/2.8804121d.chunk.css"
+  },
+  {
+    "revision": "de29ba9c83f45a848e89",
+    "url": "/Get-Movies-Info/static/css/main.448b8a6b.chunk.css"
+  },
+  {
+    "revision": "6736827938ed82624360",
+    "url": "/Get-Movies-Info/static/js/2.1ca998cc.chunk.js"
+  },
+  {
+    "revision": "38fa58e507bbf2fa7dd865d45d231628",
+    "url": "/Get-Movies-Info/static/js/2.1ca998cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de29ba9c83f45a848e89",
+    "url": "/Get-Movies-Info/static/js/main.1e5800b9.chunk.js"
+  },
+  {
+    "revision": "2fbca1679cf273c39f52",
+    "url": "/Get-Movies-Info/static/js/runtime-main.90a7f9cc.js"
+  },
+  {
+    "revision": "e143be5e901d8d9dd31e33c9bc4e806c",
+    "url": "/Get-Movies-Info/static/media/004-website-24.e143be5e.png"
+  },
+  {
+    "revision": "0ed7661fc7ec2d1f15ca5bae2f45845b",
+    "url": "/Get-Movies-Info/static/media/004-website-25.0ed7661f.png"
+  },
+  {
+    "revision": "08da5f5b6e52aadc93566f9a4a2ed7cf",
+    "url": "/Get-Movies-Info/static/media/1_Y-yWsCHUc_8Xwti2k9sW2Q.08da5f5b.jpg"
+  },
+  {
+    "revision": "6c2c02d647f05cc35ce94b78a558fa4c",
+    "url": "/Get-Movies-Info/static/media/488c9dea43b1f7f0a07652028deb9dff.6c2c02d6.jpg"
+  },
+  {
+    "revision": "7578f6ee3ab601edf63124148b62dedd",
+    "url": "/Get-Movies-Info/static/media/71hL9hv3RPL._AC_SL1405_.7578f6ee.jpg"
+  },
+  {
+    "revision": "53fcf405186535b668bc1abd1c10c714",
+    "url": "/Get-Movies-Info/static/media/7m7wa213ccc41.53fcf405.jpg"
+  },
+  {
+    "revision": "249cb52efc3d93d3c84a602274e16d61",
+    "url": "/Get-Movies-Info/static/media/Badboy.249cb52e.mp4"
+  },
+  {
+    "revision": "0d6a240f17d7eaf520760bc496e8cba3",
+    "url": "/Get-Movies-Info/static/media/Beauty-Beast-2017-Movie-Posters.0d6a240f.jpg"
+  },
+  {
+    "revision": "1063aa965910d4093a4e98bde052885c",
+    "url": "/Get-Movies-Info/static/media/Netflix_N_logo.1063aa96.png"
+  },
+  {
+    "revision": "4a805eed8923497f27731f63e7b46894",
+    "url": "/Get-Movies-Info/static/media/X-Men-Dark-Phoenix-New-Poster_1200_1777_81_s.4a805eed.jpg"
+  },
+  {
+    "revision": "fa530fe686ba60572bba3d3cf2a40eea",
+    "url": "/Get-Movies-Info/static/media/about.fa530fe6.png"
+  },
+  {
+    "revision": "bd9601884804f5e1e39d436bc05b9f63",
+    "url": "/Get-Movies-Info/static/media/black-panther-web.bd960188.jpg"
+  },
+  {
+    "revision": "17d07007c3475d7fa7ef3ec13360cc1c",
+    "url": "/Get-Movies-Info/static/media/download (1).17d07007.jpg"
+  },
+  {
+    "revision": "8b59592fe196324dba1e4c63cd029c3d",
+    "url": "/Get-Movies-Info/static/media/extraction-netflix-chris-hemsworth-poster-full.8b59592f.jpg"
+  },
+  {
+    "revision": "91b9e4c49ce26dbab2676cd53d1aaf53",
+    "url": "/Get-Movies-Info/static/media/personal11.91b9e4c4.jpg"
+  },
+  {
+    "revision": "178e1d1497feed3514c07096e49f9789",
+    "url": "/Get-Movies-Info/static/media/personal22.178e1d14.jpg"
+  },
+  {
+    "revision": "428d92d832044c435426a39e692b2895",
+    "url": "/Get-Movies-Info/static/media/personal33.428d92d8.jpg"
+  },
+  {
+    "revision": "bbd9468e88116317e13f142a0d6251a4",
+    "url": "/Get-Movies-Info/static/media/personal44.bbd9468e.jpg"
+  },
+  {
+    "revision": "366219659568ac560b221f82b0951798",
+    "url": "/Get-Movies-Info/static/media/personal55.36621965.jpg"
+  },
+  {
+    "revision": "6cd410dfeea2c4740bca24deef5291a9",
+    "url": "/Get-Movies-Info/static/media/personal66.6cd410df.jpg"
+  },
+  {
+    "revision": "4442d6502e770306a59cb344bc38779c",
+    "url": "/Get-Movies-Info/static/media/photo.4442d650.jpg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/Get-Movies-Info/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/Get-Movies-Info/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/Get-Movies-Info/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/Get-Movies-Info/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "46e7abd34002851a424499321d7922e2",
+    "url": "/Get-Movies-Info/static/media/spider-man-homecoming-movie-poster-marvel-cinematic-universe-1038913.46e7abd3.jpg"
+  },
+  {
+    "revision": "52c445381622355858e304f5db642aa7",
+    "url": "/Get-Movies-Info/static/media/star-wars-the-last-jedi-movie-poster.52c44538.jpg"
+  },
+  {
+    "revision": "8dc285d8c46bdd0aa0157c2230cf1bb5",
+    "url": "/Get-Movies-Info/static/media/super (10).8dc285d8.jpg"
+  },
+  {
+    "revision": "3e8833927814e1d7d173d7cc662bafea",
+    "url": "/Get-Movies-Info/static/media/super (11).3e883392.jpg"
+  },
+  {
+    "revision": "0d78bad6853b644fb40e0c07c6d99a4c",
+    "url": "/Get-Movies-Info/static/media/super (6).0d78bad6.jpg"
+  },
+  {
+    "revision": "b10377b2fd07744b7790b9f640f35067",
+    "url": "/Get-Movies-Info/static/media/super (7).b10377b2.jpg"
+  },
+  {
+    "revision": "29386944429318f344414b9b1feaaf78",
+    "url": "/Get-Movies-Info/static/media/super (8).29386944.jpg"
+  }
+]);
